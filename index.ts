dc4768b88c3045b7553ee9c5a0b28a4d6d5a5245
unknown <fa-ndo@hotmail.com>
2022-09-27 17:47:08 -0300

    nose si anda

--- index.ts
+++ index.ts
@@ -4,7 +4,10 @@ const port = process.env.PORT || 3000;
 console.log(process.env.FIREBASE_CONECTION);
 
 app.get("/hola", (req, res) => {
-  res.json({ message: "hola soy el servidor,heroku" });
+  res.json({
+    message:
+      "hola soy el servidor,heroku Y VAS A FUNCIONAAAAAAAARRRRRRR????????",
+  });
 });
 
 app.listen(3002, () => {
