048d6c87e5efed2dc366b2225ae45fcbf0a18213
unknown <fa-ndo@hotmail.com>
2022-09-27 17:27:00 -0300

    segundo cambio

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 console.log(process.env.FIREBASE_CONECTION);
 
 app.get("/hola", (req, res) => {
-  res.json({ message: "hola soy el servidor" });
+  res.json({ message: "hola soy el servidor,heroku" });
 });
 
 app.listen(3002, () => {
