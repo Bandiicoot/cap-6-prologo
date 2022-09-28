776f3023234f8b52f28ac66b2a39ffcdad085c19
unknown <fa-ndo@hotmail.com>
2022-09-28 14:11:10 -0300

    JULIIIII

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ exports.__esModule = true;
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
-console.log(process.env.FIREBASE_CONECTION);
+//console.log(process.env.FIREBASE_CONECTION);
 app.get("/hola", function (req, res) {
     res.json({
-        message: "hola soy el servidor,heroku Y VAS A FUNCIONAAAAAAAARRRRRRR????????"
+        message: "hola soy el servidor,heroku SOY AXELe Y ASDHAGOAAOGADSDASFGFD Y VAS A FUNCIONAAAAAAAARRRRRRR????????"
     });
 });
 app.listen(3000, function () {
